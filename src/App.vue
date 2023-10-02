6439fc407847879701ade08aa51b95d077466f18
Benrise <benrise_x@mail.ru>
2023-10-02 13:21:24 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+  import TheHeader from '@/components/TheHeader.vue'
+  import TheFooter from '@/components/TheFooter.vue'
+
+
+  export default {
+    name: 'App',
+    components:{
+        TheHeader,
+        TheFooter,
+    },
+}
+</script>
+
+<template>
+  <div class="wrapper">
+      <TheHeader />
+
+      <main class="main">
+        <router-view></router-view>
+      </main>
+        
+      <TheFooter />
+  </div>
+</template>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,28 @@
+<template>
+    <header class="header">
+        <div class="header__container">
+            <router-link to="/" class="header__logo">
+                <img
+                        src="/svg/logo/logo-shape.svg"
+                        class="header__image header__image_adaptive"
+                        alt="Alef Shape Logo"
+                />
+                <img
+                        src="/svg/logo/logo-text.svg"
+                        class="header__image header__image_adaptive"
+                        alt="Alef Text Logo"
+                />
+            </router-link>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
