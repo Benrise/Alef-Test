6db0c6ce743b09a49a7e80b577be9c0f08be7992
Benrise <benrise_x@mail.ru>
2023-10-02 16:51:07 +0300

    Header realized (adaptive)

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -4,25 +4,55 @@
             <router-link to="/" class="header__logo">
                 <img
                         src="/svg/logo/logo-shape.svg"
-                        class="header__image header__image_adaptive"
+                        class="header__shape-logo"
                         alt="Alef Shape Logo"
                 />
                 <img
                         src="/svg/logo/logo-text.svg"
-                        class="header__image header__image_adaptive"
+                        class="header__text-logo"
                         alt="Alef Text Logo"
                 />
             </router-link>
+            <nav class="header__menu menu" :class="{ '_active': isMenuOpen }">
+                <ul class="menu__list">
+                    <li class="menu__item">
+                        <router-link to="/form" class="menu__link" >
+                            Форма
+                        </router-link>
+                    </li>
+                    <li class="menu__item">
+                        <router-link to="/preview" class="menu__link" >
+                            Превью
+                        </router-link>
+                    </li>
+                </ul>
+            </nav>
+            <div class="header__menu-icon" @click="toggleMenu" :class="{ '_active': isMenuOpen }"><span></span></div>
+            <div class="header__filler">
+
+            </div>
         </div>
     </header>
 </template>
 
 <script>
     export default {
-        
+        data() {
+            return {
+                isMenuOpen: false,
+            }
+        },
+        methods: {
+            toggleMenu() {
+                document.body.classList.toggle('_lock');
+                this.isMenuOpen = !this.isMenuOpen;
+            },
+        },
+        watch: {
+            '$route' () {
+                this.isMenuOpen = false
+                document.body.classList.remove('_lock');
+            }
+        }
     }
 </script>
-
-<style lang="scss" scoped>
-
-</style>
